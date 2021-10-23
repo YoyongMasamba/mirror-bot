6448b0e5e33934e0736143ad8d63a0d847727828
uzaki19 <88629461+uzaki19@users.noreply.github.com>
2021-10-23 23:57:08 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/SlamDevs/slam-mirrorbot")
-    buttons.buildbutton("Channel", "https://t.me/SlamMirrorUpdates")
+    buttons.buildbutton("18+ group", "https://t.me/joinchat/CPTIfl7AShlmYjZl")
+    buttons.buildbutton("Channel", "https://t.me/tokisaki_mitsuha_publik)
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
