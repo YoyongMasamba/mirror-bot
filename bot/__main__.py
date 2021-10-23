b9ad8c848666caf8ab90cb80a66b2859e37610fc
uzaki19 <88629461+uzaki19@users.noreply.github.com>
2021-10-24 00:13:35 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -46,7 +46,7 @@ def stats(update, context):
 def start(update, context):
     buttons = button_build.ButtonMaker()
     buttons.buildbutton("18+ group", "https://t.me/joinchat/CPTIfl7AShlmYjZl")
-    buttons.buildbutton("Channel", "https://t.me/tokisaki_mitsuha_publik)
+    buttons.buildbutton("Channel", "https://t.me/tokisaki_mitsuha_publik")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
